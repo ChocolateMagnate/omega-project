ff9d98374c9c4559d98a6007af3b61ab95088d3b
ChocolateMagnate <pl5_kvya@student.ztu.edu.ua>
2024-12-02 11:31:44 +0200

    #1: implemented multi-dataset loading and changed the default dataset pair to WikiText2 and CodeSearchNet.

--- omega/transformer/cmd.py
+++ omega/transformer/cmd.py
@@ -6,10 +6,10 @@ OMEGA_TOKENIZER_PATH = environ.get("OMEGA_TOKENIZER_PATH")
 
 # The dataset to use to train the tokenizer. It should be representative of the kind of input the model
 # will be trained on, such as multiple natural and programming languages, and hosted on HuggingFace for
-# convenience. The default value is bigcode/starcoderdata because it provides mix of code and English
-# documentation, and for extending it into other languages, it should be changed. The number of chunks
-# to train on is configured with OMEGA_HUGGINGFACE_CHUNKS.
-OMEGA_HUGGINGFACE_DATASET = environ.get("OMEGA_HUGGINGFACE_DATASET", "bigcode/starcoderdata")
+# convenience. Multiple datasets can be enumerated with colon (:). The default dataset configuration is
+# the mix of WikiText2 and CodeSearchNet that gives model strong English and programming tokenization.
+# The number of chunks to train on is configured with OMEGA_HUGGINGFACE_CHUNKS.
+OMEGA_HUGGINGFACE_DATASETS = environ.get("OMEGA_HUGGINGFACE_DATASET", "mindchain/wikitext2:code-search-net/code_search_net")
 
 # The number of chunks to use to train the model from the OMEGA_HUGGINGFACE_DATASET dataset.
 OMEGA_HUGGINGFACE_CHUNKS = int(environ.get("OMEGA_HUGGINGFACE_CHUNKS", 100000))
--- omega/transformer/tokenizer.py
+++ omega/transformer/tokenizer.py
@@ -1,28 +1,35 @@
-import torch
-import datasets
+import os
 import multiprocessing
-from torch import Tensor
+
+import torch
 import sentencepiece as spm
+import datasets as ds
 
 import omega.transformer.cmd as cmd
-from omega.transformer.typing import Matrix
+from omega.transformer.typing import Vector, Matrix
 
-if cmd.OMEGA_TOKENIZER_DATA is not None:
-    dataset = datasets.load_dataset(
-        cmd.OMEGA_HUGGINGFACE_DATASET,
-        split=f"train[:{cmd.OMEGA_HUGGINGFACE_CHUNKS}]",
-        streaming=True,
-    )
-    dataset.to_json(cmd.OMEGA_TOKENIZER_DATA, lines=True)
+
+def fit_tokenizer_model():
+    datum = []
+    datasets = cmd.OMEGA_HUGGINGFACE_DATASETS.split(":")
+    for dataset in datasets:
+        data = ds.load_dataset(
+            dataset,
+            split=f"train[:{cmd.OMEGA_HUGGINGFACE_CHUNKS}]",
+            streaming=True,
+        )
+        datum.append(data)
+    mixed_dataset = ds.concatenate_datasets(datum)
+    mixed_dataset.to_json(cmd.OMEGA_TOKENIZER_DATA, lines=True)
 
     spm.SentencePieceTrainer.Train(
         input=cmd.OMEGA_TOKENIZER_DATA,
         model_type=cmd.OMEGA_TOKENIZER_MODEL_TYPE,
         vocab_size=cmd.OMEGA_TOKENIZER_VOCABULARY_SIZE,
         pad_id=0, unk_id=1, bos_id=2, eos_id=3,
-        pad_piece="[PAD]", unk_piece="[UNK]",  bos_piece="[BOS]", eos_piece="[EOS]",
+        pad_piece="[PAD]", unk_piece="[UNK]", bos_piece="[BOS]", eos_piece="[EOS]",
         character_coverage=1.0,
-        num_threads = multiprocessing.cpu_count(),
+        num_threads=multiprocessing.cpu_count(),
         split_digits=True,
         byte_fallback=True,
         allow_whitespace_only_pieces=False,
@@ -35,12 +42,16 @@ class OmegaTokenizer:
         self.sp = spm.SentencePieceProcessor()
         self.sp.Load(cmd.OMEGA_TOKENIZER_PATH + ":" + tag)
 
-    def encode(self, texts: list[str]) -> Tensor:
+    def encode(self, text: str) -> Matrix:
         return torch.nn.utils.rnn.pad_sequence(
-            [torch.tensor(self.sp.Encode(text)) for text in texts],
+            torch.tensor(self.sp.Encode(text)),
             batch_first=True,
             padding_value=self.sp.pad_id()
         )
 
-    def decode(self, output: Matrix) -> list[str]:
-        return [self.sp.Decode(batch) for batch in output]
+    def decode(self, output: Vector) -> str:
+        return self.sp.Decode(output)
+
+
+if cmd.OMEGA_TOKENIZER_DATA is not None and not os.path.exists(cmd.OMEGA_TOKENIZER_PATH):
+    fit_tokenizer_model()
